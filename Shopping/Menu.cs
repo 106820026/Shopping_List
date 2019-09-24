418192af6b63f4fd15bdde344d6fd4ffc4856977
106820026 <i8807701314520@yahoo.com.tw>
2019-09-24 16:47:38 +0800

    disable button when the window is open and enable when the window is close in Menu

--- Shopping/Menu.cs
+++ Shopping/Menu.cs
@@ -18,28 +18,42 @@ namespace ShopList
         public Menu()
         {
             InitializeComponent();
-            _shopList = new ShopList();
-            _inventorySystem = new InventorySystem();
         }
 
         // 開啟網購視窗
         private void _orderSystemButtonClick(object sender, EventArgs e)
         {
+            _shopList = new ShopList();
             _shopList.Show();
             _orderSystemButton.Enabled = false;
+            this._shopList.FormClosed += ResetOrderButton;
         }
 
         // 開啟資料庫
         private void _inventorySystemButtonClick(object sender, EventArgs e)
         {
+            _inventorySystem = new InventorySystem();
             _inventorySystem.Show();
             _inventorySystemButton.Enabled = false;
+            this._inventorySystem.FormClosed += ResetInventoryButton;
         }
 
         // 關閉程式
         private void _exitButtonClick(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        // 使按鈕可再次使用 
+        private void ResetOrderButton(object sender, EventArgs e)
+        {
+            _orderSystemButton.Enabled = true;
+        }
+
+        // 使按鈕可再次使用
+        private void ResetInventoryButton(object sender, EventArgs e)
+        {
+            _inventorySystemButton.Enabled = true;
         }
     }
 }
